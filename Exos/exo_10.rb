6aed80f254f004d6a0c0cb3ddc2a3a167791f376
Emilie <emilie.aussage@gmail.com>
2020-04-28 22:21:24 +0200

    Exo 10 bof

---
+++ Exos/exo_10.rb
@@ -0,0 +1,6 @@
+puts "Entrez année de naissance"
+print "> "
+number_age = gets.chomp
+number_year = 2017
+
+puts "Vous avez eu #{number_year - number_age } ans en 2017! Vous êtes un peu jeune pour sortir avec moi..." 
