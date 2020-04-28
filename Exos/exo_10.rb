94157e0546418654736ce76c686abf4e57e234a2
Emilie <emilie.aussage@gmail.com>
2020-04-29 00:23:04 +0200

    add last document

--- Exos/exo_10.rb
+++ Exos/exo_10.rb
@@ -1,6 +1,6 @@
 puts "Entrez année de naissance"
 print "> "
-number_age = gets.chomp
-number_year = 2017
+number  = gets.chomp.to_i
+a = 2017
 
-puts "Vous avez eu #{number_year - number_age } ans en 2017! Vous êtes un peu jeune pour sortir avec moi..." 
+puts "Vous avez eu #{a - number } ans en 2017! Vous êtes un peu jeune pour sortir avec moi..." 
