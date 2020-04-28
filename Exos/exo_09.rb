eeaf0791a563dbfd1477916f388d9207f7761a4b
Emilie <emilie.aussage@gmail.com>
2020-04-28 22:20:50 +0200

    exo 9 OK

---
+++ Exos/exo_09.rb
@@ -0,0 +1,7 @@
+puts "Entrez votre prénom"
+print "> "
+user_first_name = gets.chomp
+puts "Entrez votre nom de famille"
+print "> "
+user_name = gets.chomp
+puts "Bonjour, #{user_first_name} #{user_name}!" 
