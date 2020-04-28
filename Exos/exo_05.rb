002039a943845ae60bf21d0437bce7fa62a5815a
Emilie <emilie.aussage@gmail.com>
2020-04-28 16:55:23 +0200

    Exo 5 ok

---
+++ Exos/exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" #insertion d'une phrase
+puts "Travail : #{10 * 5 * 11}" #demande à l'application de faire une multiplication
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #même procédé qu'au dessus
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60 #Manière différente de faire un calcul que celle au dessus
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7 #On a demandé si le résultat en haut était "vrai". Etant donné que 5 n'est pas plus petit que 2 alors la réponse est "false"
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #demande de calcul
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # tout pareil
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #On reformule sous forme de fonction la demande
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
